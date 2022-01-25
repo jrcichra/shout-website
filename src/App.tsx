63e63d811df0e3e7f343b326890649448c472a50
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-01-25 01:03:38 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const baseURL = 'https://shout.jrcichra.dev';
+
+
+function isValidHttpUrl(string: string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [response, setResponse] = useState('');
+  const [go, setGo] = useState(false);
+
+  useEffect(() => {
+    if (go) {
+      setGo(false);
+      setResponse('');
+      if (isValidHttpUrl(url)) {
+        fetch(`${baseURL}/api/`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            url: url
+          })
+        })
+          .then(res => res.json())
+          .then(res => {
+            setResponse(`${baseURL}/${res.word}`);
+          })
+      } else {
+        setResponse(`'${url}': Invalid URL`);
+      }
+    }
+  }, [go]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>shout</p>
+        <input type="text" placeholder='https://google.com' value={url} onChange={(e) => setUrl(e.target.value)} />
+        <br></br>
+        <input type="button" value="Add" onClick={() => setGo(true)} />
+        <p>{response}</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
