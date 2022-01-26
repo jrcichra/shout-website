0a6ba7e0af49d8b0a6290a3795818561fc69bce2
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-01-25 22:41:15 -0500

    add TTL and add dev proxy

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ function isValidHttpUrl(string: string) {
   return url.protocol === "http:" || url.protocol === "https:";
 }
 
-
 function App() {
   const [url, setUrl] = useState('');
   const [response, setResponse] = useState('');
+  const [ttl, setTtl]: [any, any] = useState(undefined);
   const [go, setGo] = useState(false);
 
   useEffect(() => {
@@ -28,7 +28,8 @@ function App() {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            url: url
+            url: url,
+            ttl: ttl === undefined ? ttl : ttl * 60,
           })
         })
           .then(res => res.json())
@@ -41,13 +42,22 @@ function App() {
     }
   }, [go]);
 
+  function handleKeyPress(e: any) {
+    if (e.key === 'Enter') {
+      setGo(true);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>shout</p>
-        <input type="text" placeholder='https://google.com' value={url} onChange={(e) => setUrl(e.target.value)} />
+        <input type="text" placeholder='https://google.com' value={url} onChange={(e) => setUrl(e.target.value)} onKeyPress={handleKeyPress} />
         <br></br>
-        <input type="button" value="Add" onClick={() => setGo(true)} />
+        <input id="ttl" type="text" placeholder='300' value={ttl} onChange={(e) => setTtl(Number(e.target.value))} onKeyPress={handleKeyPress} />
+        <br></br>
+        <input type="button" value="Shout" onClick={() => setGo(true)} />
+        <br></br>
         <a href={response}>{response}</a>
       </header>
     </div>
